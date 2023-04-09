7c6f58d67370bc2d30973f386d27cfe5e1dc9feb
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-04-09 17:25:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+<script>
+        function loginform()
+        {
+            var fname=document.getElementById("fname").value;
+            var lname=document.getElementById("lname").value;
+            var pno=document.getElementById("pno").value;
+            var emailid=document.getElementById("emailid").value;
+            alert("First Name: "+fname+"\n"+"Last Name: "+lname+"\n"+"Phone Number: "+pno+"\n"+"Email ID: "+emailid);
+        }
+    </script>
